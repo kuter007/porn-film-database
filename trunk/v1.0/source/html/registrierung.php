6b3f7612a1286e9fa63a2754636e00f3a5ad8331
fatlind.ameti@gmail.com <fatlind.ameti@gmail.com@80df670a-5ce5-08fd-2ce2-6ac694042b08>
2012-11-02 07:24:33 +0000

    Einfügen des Templates (HTML-Source)

--- trunk/v1.0/source/html/registrierung.php
+++ trunk/v1.0/source/html/registrierung.php
@@ -1,18 +1,59 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Sensual Movie Doc</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.1.min.js"></script>
     </head>
-    <body> 
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <!-- This code is taken from http://twitter.github.com/bootstrap/examples/hero.html -->
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="#">Sensual Movie Doc</a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#about">Filmlist</a></li>
+                            <li><a href="#contact">Go Pro</a></li>
+                            <li><a href="#login">Log-In</a></li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+		<div class="hero-unit">
         <?php
         include 'access.php';
          
-        //Save input field Values in Variable 
+        //Save input field Values in Variable
         $vorname = $_POST['vorname'];
         $nachname = $_POST['nachname'];
         $strasse = $_POST['strasse'];
@@ -22,23 +63,23 @@ and open the template in the editor.
         $gdatum = $_POST['gdatum'];
         $email = $_POST['email'];
         $senden = $_POST['senden'];
-        
-        //Select DB 
+       
+        //Select DB
         mysql_select_db($database);
-        
+       
         if (!$connection){
             die('Could not connect: ' . mysql_error());
         }else{      
-        
+       
             if ($senden != ""){
                 //is exist email OR vorname do not save
                 $ifExistEmail = "SELECT email FROM Kunde WHERE email = '$email'";
                 $emailQuery = mysql_query($ifExistEmail) or die (mysql_error());    
-                $emailResult = mysql_fetch_array($emailQuery);                 
+                $emailResult = mysql_fetch_array($emailQuery);                
                 $ifExistVorname = "SELECT vorname FROM Anschrift WHERE vorname = '$vorname'";
                 $vornameQuery = mysql_query($ifExistVorname) or die (mysql_error());
                 $vornameResult = mysql_fetch_array($vornameQuery);
-                
+               
                 if($emailResult['email'] == $email OR $vornameResult['vorname'] == $vorname){
                     echo '<b style="color:red">Die E-Mail Adresse oder Benutzername ist bereits vorhanden Kunde mit der email ist bereits vorhanden</b>';
                 }else{
@@ -47,12 +88,12 @@ and open the template in the editor.
                     mysql_query($insertTableAnschrift) or die (mysql_error());
                     //Get last inserted ID to write in Kunde.anschriftId                
                     $anschriftId = mysql_insert_id();
-                    //Query to insert into Kunde 
+                    //Query to insert into Kunde
                     $insertTableKunde = "INSERT INTO Kunde (anschriftId,geburtsdatum,email) VALUES('$anschriftId','$gdatum','$email')";
                     $successReg = mysql_query($insertTableKunde) or die (mysql_error());    
-                
+               
                 if($successReg){
-                    echo 'Speichern Erfolgreich';                     
+                    echo 'Speichern Erfolgreich';                    
                     }
                 }
             }
@@ -82,7 +123,7 @@ and open the template in the editor.
             </tr>
             <tr>
                 <td>
-                    strasse
+                    Stra&szlig;e
                 </td>
                 <td>
                     <input type="text" name="strasse" />
@@ -90,7 +131,7 @@ and open the template in the editor.
             </tr>
             <tr>
                 <td>
-                    hausnummer
+                    Hausnummer
                 </td>
                 <td>
                     <input type="text" name="hausnummer" />
@@ -98,7 +139,7 @@ and open the template in the editor.
             </tr>
             <tr>
                 <td>
-                    postleitzahl
+                    Postleitzahl
                 </td>
                 <td>
                     <input type="text" name="plz" />
@@ -122,7 +163,7 @@ and open the template in the editor.
             </tr>
             <tr>
                 <td>
-                    email
+                    Email
                 </td>
                 <td>
                     <input type="text" name="email" />
@@ -133,5 +174,7 @@ and open the template in the editor.
             </tr>
         </table>
         </form>
+        </div>
+        <div class="row">&nbsp;</div>
     </body>
 </html>
